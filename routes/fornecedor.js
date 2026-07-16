51902909f62b57ff0c62b63e5896a36bd5af36c9
LRuthes907 <lucasruthes907@gmail.com>
2026-07-16 12:15:27 -0300

    Adiciona conexão com banco de dados e atualiza rotas de produtos, fornecedor e produção

--- routes/fornecedor.js
+++ routes/fornecedor.js
@@ -1,33 +1,28 @@
 import express from 'express';
 const router = express.Router();
+import db from '../db.js';
 
-const fornecedores = [
-    { id: 1, nome: 'Moinho São João', cnpj: '12.345.678/0001-90', telefone: '(11) 1234-5678', email: 'contato@moinhosaojoao.com', insumos: 'farinha' },
-    { id: 2, nome: 'Molhos e Cia', cnpj: '98.765.432/0001-10', telefone: '(11) 8765-4321', email: 'contato@molhosecia.com', insumos: 'molho de tomate' },
-    { id: 3, nome: 'Laticínios do Campo', cnpj: '11.222.333/0001-44', telefone: '(11) 1122-3344', email: 'contato@laticiniosdocampo.com', insumos: 'queijo' }
-];
 
 
 //Retorna a lista completa de todos os fornecedores cadastrados no sistema.
-router.get('/', (req, res) => {
-    res.json(fornecedores);
+router.get('/', async (req, res) => {
+    const [rows] = await db.query('SELECT * FROM fornecedores');
+
+    res.json(rows);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-    const fornecedor = fornecedores.find(f => f.id === id);
+    const [rows] = await db.query('SELECT * FROM fornecedores WHERE id = ?', [id]);
 
-    if (!fornecedor) {
+    if (rows.length === 0) {
         return res.status(404).json({ error: 'Fornecedor não encontrado.' });
     }
-
-    res.json(fornecedor);
+    res.json(rows[0]);
 });
 
-
 //cadastro de um novo fornecedor no sistema.
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
     const camposObrigatorios = ['nome', 'cnpj', 'telefone', 'email', 'insumos'];
     const faltando = camposObrigatorios.find(campo => !req.body[campo]);
 
@@ -35,45 +30,81 @@ router.post('/', (req, res) => {
         return res.status(400).json({ error: `O campo ${faltando} é obrigatório.` });
     }
 
-    const id = fornecedores.length + 1;
-    const novoFornecedor = {
-        id: id,
-        nome: req.body.nome,
-        cnpj: req.body.cnpj,
-        telefone: req.body.telefone,
-        email: req.body.email,
-        insumos: req.body.insumos
-    };
-    fornecedores.push(novoFornecedor);
-    res.status(201).json(novoFornecedor);
+
+    const [result] = await db.query('INSERT INTO fornecedores (nome, cnpj, telefone, email, insumos) VALUES (?,?,?,?,?)', [
+        req.body.nome,
+        req.body.cnpj,
+        req.body.telefone,
+        req.body.email,
+        req.body.insumos
+    ]
+    );
+
+    const idGerado = result.insertId;
+
+    const [rows] = await db.query('SELECT * FROM fornecedores WHERE id = ?', [idGerado]);
+
+    res.status(201).json(rows[0]);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
 
-    const fornecedorId = parseInt(req.params.id);
-    const fornecedor = fornecedores.find(f => f.id === fornecedorId);
+    const campos = [];
+    const valores = [];
+    const id = parseInt(req.params.id);
 
-    if (!fornecedor) {
+    if (req.body.nome !== undefined) {
+        campos.push('nome = ?');
+        valores.push(req.body.nome);
+    }
+    if (req.body.cnpj !== undefined) {
+        campos.push('cnpj = ?');
+        valores.push(req.body.cnpj);
+    }
+    if (req.body.telefone !== undefined) {
+        campos.push('telefone = ?');
+        valores.push(req.body.telefone);
+    }
+    if (req.body.email !== undefined) {
+        campos.push('email = ?');
+        valores.push(req.body.email);
+    }
+    if (req.body.insumos !== undefined) {
+        campos.push('insumos = ?');
+        valores.push(req.body.insumos);
+    }
+
+    if (campos.length === 0) {
+        return res.status(400).json({ error: 'Nenhum campo para atualizar.' });
+    }
+
+    const set = campos.join(', ');
+
+    const query = `UPDATE fornecedores SET ${set} WHERE id = ?`;
+    valores.push(id);
+
+    const [result] = await db.query(query, valores);
+
+    if (result.affectedRows === 0) {
         return res.status(404).json({ error: 'Fornecedor não encontrado.' });
     }
 
-    fornecedor.nome = req.body.nome || fornecedor.nome;
-    fornecedor.cnpj = req.body.cnpj || fornecedor.cnpj;
-    fornecedor.telefone = req.body.telefone || fornecedor.telefone;
-    fornecedor.email = req.body.email || fornecedor.email;
-    fornecedor.insumos = req.body.insumos || fornecedor.insumos;
-    res.json(fornecedor);
+    const [rows] = await db.query('SELECT * FROM fornecedores WHERE id = ?', [id]);
+    res.status(200).json(rows[0]);
+
+
+
 });
 
-router.delete('/:id', (req, res) => {
-    const fornecedorId = parseInt(req.params.id);
-    const index = fornecedores.findIndex(f => f.id === fornecedorId);
+router.delete('/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
 
-    if (index === -1) {
+    const [result] = await db.query('DELETE FROM fornecedores WHERE id = ?', [id]);
+
+    if (result.affectedRows === 0) {
         return res.status(404).json({ error: 'Fornecedor não encontrado.' });
     }
 
-    fornecedores.splice(index, 1);
     res.status(204).send();
 });
 
